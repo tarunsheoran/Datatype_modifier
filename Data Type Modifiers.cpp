d481a6bed89d325e5592b4df016e33edea5f8ca6
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-06-22 22:12:05 +0530

    Add files via upload

---
+++ Data Type Modifiers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    
+    short int si;
+    long int li;
+    cout<< "Size of longint "<<sizeof(li)<<endl;
+       cout<< "Size of shortint "<<sizeof(si)<<endl;
+       return 0;
+}
